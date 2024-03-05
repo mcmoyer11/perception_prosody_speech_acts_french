53aa7a39fb317dcb3682e3a0f20c052c9ef94136
Morgan Moyer <mcmoyer11@gmail.com>
2024-03-05 14:05:06 +0100

    testing gemerate_stim()

--- js/stimuli.js
+++ js/stimuli.js
@@ -12,46 +12,75 @@ function get_data(fullpath) {
     return response.responseText;
 }
 
-function generate_stim() {
+// function generate_stim() {
 
+//     var contents = get_data(path);
+//     var raw = contents;
+//     var rows = raw.split('\n');
+//     var data = [];
+
+//     headings = rows[0].split('\t');
+//     var total = rows.length - 1;
+
+//     // Push every item from the text file into the array, split on the tab (or whatever split)
+//     for (var i = 1; i < total; i++) {
+//         data.push(rows[i].split('\t'));
+//     }
+
+//     console.log("data: ", data);
+
+//     var stim = data.map(function (row) {
+//         return row.reduce(function (result, field, index) {
+//             result[headings[index]] = field;
+//             return result;
+//         }, {});
+//     });
+
+//     console.log("stim: ", stim);
+
+//     return stim;
+
+//     // Push every item from the text file into the array, split on the tab (or whatever split)
+//     for (var i = 1; i < total; i++) {   
+//         var rowData = rows[i].split('\t');
+//         var rowObject = {};
+//         for (var j = 0; j < headings.length; j++) {
+//             rowObject[headings[j]] = rowData[j];
+//         }
+//         data.push(rowObject);
+//     } 
+
+//     console.log("data: ", data);
+
+//     return data;
+
+// }
+
+function generate_stim() {
     var contents = get_data(path);
-    var raw = contents;
-    var rows = raw.split('\n');
+    var rows = contents.split('\n');
     var data = [];
 
-    headings = rows[0].split('\t');
-    var total = rows.length - 1;
+    // Extract column names from the first row
+    var headings = rows[0].split(',');
 
-    // Push every item from the text file into the array, split on the tab (or whatever split)
-    // for (var i = 1; i < total; i++) {
-    //     data.push(rows[i].split('\t'));
-    // }
+    // Loop through each row starting from the second row
+    for (var i = 1; i < rows.length; i++) {
+        var rowData = rows[i].split(',');
 
-    // console.log("data: ", data);
-
-    // var stim = data.map(function (row) {
-    //     return row.reduce(function (result, field, index) {
-    //         result[headings[index]] = field;
-    //         return result;
-    //     }, {});
-    // });
-
-    // console.log("stim: ", stim);
-
-    // return stim;
-
-    // Push every item from the text file into the array, split on the tab (or whatever split)
-    for (var i = 1; i < total; i++) {   
-        var rowData = rows[i].split('\t');
+        // Create an object to store data for each row
         var rowObject = {};
+
+        // Loop through each column and assign the value to the corresponding column name
         for (var j = 0; j < headings.length; j++) {
             rowObject[headings[j]] = rowData[j];
         }
+
+        // Push the row object to the data array
         data.push(rowObject);
-    } 
+    }
 
     console.log("data: ", data);
 
     return data;
-
 }
